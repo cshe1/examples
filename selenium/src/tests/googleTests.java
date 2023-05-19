a4143393d9ceb8b967d82b82ae18417299f83d40
Cory She <coryzshe@gmail.com>
2023-05-19 00:27:14 -0400

    Added new initialize options to fix selenium chrome webdriver issue. Remove when fixed. Also changed of searchGoogle from input to textarea. Started work on I'm feeling lucky search method

--- selenium/src/tests/googleTests.java
+++ selenium/src/tests/googleTests.java
@@ -6,10 +6,18 @@ public class googleTests {
 
     //Change the variable for different Google search results
     String searchTerm = "Lancelot";
-
+    String luckyButtonID = "gbqfbb";
     @Test
     public void searchGoogle(){
+        utils.initializeOptions();
         utils.navigateGoogle();
         utils.searchGoogle(searchTerm);
+    }
+
+    @Test
+    public void searchGoogleLucky(){
+        utils.initializeOptions();
+        utils.navigateGoogle();
+        utils.tapButton(luckyButtonID);
     }
 }
--- selenium/src/tests/utils.java
+++ selenium/src/tests/utils.java
@@ -5,12 +5,24 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 
 public class utils {
-    static WebDriver driver = new ChromeDriver();
+
+    static WebDriver driver;
+    static ChromeOptions options = new ChromeOptions();
+
+    //Reusable variables
     static String searchBar = "gLFyf";
 
+
+    //Selenium broke google driver in update. Tried to go to the latest version but didn't
+    //Added method to get it to start. Once Selenium fixes it, remove method.
+    public static void initializeOptions(){
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+    }
     public static void navigateGoogle(){
         driver.get("https://google.com");
     }
@@ -18,8 +30,13 @@ public class utils {
     public static String searchGoogle(String input){
         driver.findElement(By.className(searchBar)).sendKeys(input + Keys.ENTER);
         //WebElement element = driver.findElement(By.xpath("//input[@value='" + input + "']"));
-        Assert.assertEquals(driver.findElement(By.xpath("//input[@value='" + input + "' and @name='q' and @aria-label= 'Search']")).isDisplayed(), true);
+        Assert.assertEquals(driver.findElement(By.xpath("//textarea[@value='" + input + "' and @name='q' and @aria-label= 'Search']")).isDisplayed(), true);
         return input;
+    }
+
+    public static String tapButton (String button){
+        driver.findElement(By.id(button)).click();
+        return button;
     }
 
     public static void validateSearch(){
